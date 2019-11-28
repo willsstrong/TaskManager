92f5e9695a2dea1dbfc438447ae25c58441acfe7
Will Strong <38888375+willsstrong@users.noreply.github.com>
2019-11-28 14:14:43 -0700

    UI Design: Add_Task Screen

--- DAL/TaskManContext.cs
+++ DAL/TaskManContext.cs
@@ -7,21 +7,15 @@ namespace Task_Manager.DAL
 
     public class TaskManContext : DbContext
     {
-        // Your context has been configured to use a 'TaskManContext' connection string from your application's 
-        // configuration file (App.config or Web.config). By default, this connection string targets the 
-        // 'Task_Manager.DAL.TaskManContext' database on your LocalDb instance. 
-        // 
-        // If you wish to target a different database and/or database provider, modify the 'TaskManContext' 
-        // connection string in the application configuration file.
-        public TaskManContext()
-            : base("name=TaskManContext")
+        public TaskManContext() 
+            : base("TaskManContext")
         {
         }
 
         // Add a DbSet for each entity type that you want to include in your model. For more information 
         // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
 
-        public virtual DbSet<Task> MyEntities { get; set; }
+        public virtual DbSet<Task> Task { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
